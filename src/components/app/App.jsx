87ca922fdb9d0913195ad493105b3788ed39a71e
Larimar4you <Zajanezlaja@gmail.com>
2025-03-29 23:58:40 +0100

    k

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,6 +3,7 @@ import s from './App.module.css';
 import ContactForm from '../contactForm/ContactForm';
 import SearchBox from '../searchBox/SearchBox';
 import ContactList from '../contactList/ContactList';
+i;
 
 const App = () => {
   return <div>App</div>;
