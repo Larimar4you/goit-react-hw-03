b943f6aec441ff4157e5e42b50786535b82f5a4d
Larimar4you <Zajanezlaja@gmail.com>
2025-03-31 23:36:47 +0200

    l

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import ContactForm from './components/ContactForm/ContactForm.module';
+import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
 import { Formik, useFormik } from 'formik';
